939e6c18c8ef62f5843f08c161795cd351d19457
Kyunghun Jeon <penpen787@gmail.com>
2016-01-14 17:37:21 +0900

    simple Class example - 2

--- etc/ClassTest.py
+++ etc/ClassTest.py
@@ -1,6 +1,9 @@
+# Example from https://wikidocs.net/28
 #  Only structure
+
+
 class Simple:
-    pass
+    pass  # 'pass' does nothing
 
 
 class EasyClass(object):
@@ -15,6 +18,16 @@ class EasyClass(object):
     def add(self):
         return self.v1 + self.v2
 
+#  in Python. 'self' indicates the instance itself
+class  Service:
+    secret = "영구는 배꼽이 두개"
+
+    def sum(self, a, b):
+        result = a + b
+        print("Hey %s , %s + %s = %s" % (self.name, a, b, result))
+
+    def setName(self, name):
+        self.name = name
 
 simple = Simple()
 simple.v1 = "aa"
@@ -25,3 +38,18 @@ c1 = EasyClass(3, 5)
 result = c1.add()
 print(c1.simpleVariable)
 print(result)
+
+id = Service()
+id.setName("Hun")
+id.sum(2, 5)
+
+Service.sum(id, 3, 7)  # call method from the Class directly, need to indicate the target instance
+
+print(type(Service()))
+
+
+id.addedField = "addedFIELD"
+print(id.addedField)
+
+id2 = Service()
+#  print(id2.addedField)  -- Error , no field
---
+++ etc/ClassTest2.py
@@ -0,0 +1,9 @@
+# Example from https://wikidocs.net/28
+class FourCal:
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+
+
+a1 = FourCal()
+a1.setdata(3, 7)
